6b984ea3b418b22141de0c6577bd8453460ac7ff
j0303p <j0303p@gmail.com>
2024-09-05 17:56:40 +0900

    feat: 초기 설정

---
+++ LawlessZone/src/main/java/dev/lawlesszone/LawlessZoneApplication.java
@@ -0,0 +1,13 @@
+package dev.lawlesszone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawlessZoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LawlessZoneApplication.class, args);
+    }
+
+}
