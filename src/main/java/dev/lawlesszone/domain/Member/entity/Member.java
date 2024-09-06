fad4b6d4144a2d4179c463acbe2cc20abdeaa65e
Kee0304 <kee0304@gmail.com>
2024-09-06 09:55:27 +0900

    [#5/fix]회원 엔티티 설정

--- src/main/java/dev/lawlesszone/domain/Member/entity/Member.java
+++ src/main/java/dev/lawlesszone/domain/Member/entity/Member.java
@@ -1,8 +1,10 @@
 package dev.lawlesszone.domain.Member.entity;
 
+import dev.lawlesszone.domain.comment.entity.Comment;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.data.annotation.Id;
 
 import javax.persistence.Entity;
@@ -12,10 +14,11 @@ import javax.persistence.OneToMany;
 
 import dev.lawlesszone.domain.atricle.entity.Article;
 
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
-
 
 @Entity
 public class Member {
@@ -27,11 +30,13 @@ public class Member {
 
     private String password;
 
-    @OneToMany
-    private Article[] articles;
+    @Lazy
+    @OneToMany(mappedBy = "author")
+    private List<Article> articles;
 
-    @OneToMany
-    private Comment[] comments;
+    @Lazy
+    @OneToMany(mappedBy = "author")
+    private List<Comment> comments;
 
     private String nickName;
 
