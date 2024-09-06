b7393e01ecbb19cd9994ad40997af0353433bcc3
j0303p <j0303p@gmail.com>
2024-09-06 10:47:08 +0900

    #4/fix 기타 설정

--- src/main/java/dev/lawlesszone/domain/payment/controller/PaymentController.java
+++ src/main/java/dev/lawlesszone/domain/payment/controller/PaymentController.java
@@ -1,12 +1,30 @@
 package dev.lawlesszone.domain.payment.controller;
 
 
+import com.siot.IamportRestClient.IamportClient;
+import com.siot.IamportRestClient.exception.IamportResponseException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
 
 @Controller()
 @RequestMapping("/payment")
 @RequiredArgsConstructor
+@Slf4j
 public class PaymentController {
+
+    private final IamportClient iamportClient;
+
+    @PostMapping("/verify/{id}")
+    public String payment(@PathVariable Long id, Model model) throws IamportResponseException, IOException {
+        log.info(String.valueOf(iamportClient.paymentByImpUid(String.valueOf(id))));
+        return "payment";
+    }
 }
--- src/main/java/dev/lawlesszone/global/config/AppConfig.java
+++ src/main/java/dev/lawlesszone/global/config/AppConfig.java
@@ -7,8 +7,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
-    String apiKey = "REST API Key를 입력합니다.";
-    String secretKey = "REST API Secret를 입력합니다.";
+    String apiKey = "8710522058100173";
+    String secretKey = "Qpobt6L7MZz8PK3567wGExmjLZ0E87nmk6Z0ziuQzYmriKVI7ea9UhwaRkODYVaGA4MmHuLmmsXbyiQE";
 
     @Bean
     public IamportClient iamportClient() {
