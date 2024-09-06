ed706dc26d6a0a88b24e79988cd91114485fa865
Kong-E <kng001016@dgu.ac.kr>
2024-09-06 19:58:15 +0900

    [#7/feat] Comment에 PreAuthorize 설정

--- src/main/java/dev/lawlesszone/domain/comment/controller/CommentController.java
+++ src/main/java/dev/lawlesszone/domain/comment/controller/CommentController.java
@@ -1,9 +1,9 @@
 package dev.lawlesszone.domain.comment.controller;
 
 import dev.lawlesszone.domain.comment.dto.CommentDTO;
-import dev.lawlesszone.domain.comment.entity.Comment;
 import dev.lawlesszone.domain.comment.service.CommentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,18 +14,21 @@ public class CommentController {
 
     private final CommentService commentService;
 
+    @PreAuthorize("isAuthenticated()")
     @PostMapping
     public String addComment(@PathVariable Long articleId, CommentDTO comment) {
         commentService.saveComment(comment, articleId);
         return "redirect:/articles/view/" + articleId;
     }
 
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("/delete/{commentId}")
     public String deleteComment(@PathVariable Long articleId, @PathVariable Long commentId) {
         commentService.deleteComment(commentId);
         return "redirect:/articles/view/" + articleId;
     }
 
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("/update/{commentId}")
     public String updateComment(@PathVariable Long articleId, @PathVariable Long commentId, CommentDTO comment) {
         commentService.updateComment(comment, commentId);
