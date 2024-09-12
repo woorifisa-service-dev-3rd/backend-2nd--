8bc1967b7382e84b70e75efaf7235807faa2e9b0
Kong-E <kng001016@dgu.ac.kr>
2024-09-12 19:19:55 +0900

    [#7/fix] CommentResponseDTO 필드 추가

--- src/main/java/dev/lawlesszone/domain/comment/dto/CommentResponseDTO.java
+++ src/main/java/dev/lawlesszone/domain/comment/dto/CommentResponseDTO.java
@@ -12,6 +12,8 @@ import javax.validation.constraints.NotNull;
 @Builder
 public class CommentResponseDTO {
 
+    private Long id;
+
     private String content;
 
     private String nickName;
@@ -22,6 +24,7 @@ public class CommentResponseDTO {
 
     public static CommentResponseDTO from(Comment comment) {
         return CommentResponseDTO.builder()
+                .id(comment.getId())
                 .content(comment.getContent())
                 .nickName((comment.getAuthor() != null) ? comment.getAuthor().getNickName() : null)
                 .email((comment.getAuthor() != null) ? comment.getAuthor().getEmail() : null)
